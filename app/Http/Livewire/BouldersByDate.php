ca60dffdd31e8d2aae4185dfc5f7e6ac172180e8
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-03-30 13:17:33 +0200

    Show all sessions in all gyms for all climber

--- app/Http/Livewire/BouldersByDate.php
+++ app/Http/Livewire/BouldersByDate.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use Cache;
 use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 use RubenVanErk\TopLoggerPhpSdk\TopLogger;
 
 class BouldersByDate extends Component
 {
-    public Collection $climbs;
-    public Collection $ascends;
     public Collection $ascendsByDate;
-    public string $userId = '';
-    public int $gymId = 8;
 
     public array $subgrades = [
         0 => 'a',
@@ -24,48 +22,86 @@ class BouldersByDate extends Component
         5 => 'c+',
     ];
 
-    public function mount()
+    public array $gyms = [
+        3 => 'Monk Rotterdam',
+        8 => 'Bruut',
+        123 => 'Bolder Neoliet',
+        131 => 'Mountain Network Dordrecht',
+    ];
+
+    public array $climberUids = [
+        '7163205870' => 'Ruben',
+        '2943357766' => 'Wouter',
+    ];
+
+    public array $climberIds = [
+        '104045' => 'Ruben',
+        '106235' => 'Wouter',
+    ];
+
+    public function mount(): void
     {
-        $this->ascendsByDate = collect();
+        $this->createDataset();
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.boulders-by-date');
     }
 
-    public function updated()
-    {
-        if (!$this->userId || !$this->gymId) {
-            return;
-        }
-        $this->ascends = $this->getAscends();
-
-        $groupedAscends = $this->ascends;
-        $groupedAscends = $groupedAscends->map(function ($ascend) {
-            $grade = (float)$ascend->climb->grade;
-            $mainGrade = (string)floor($grade);
-            $subGrade = $this->subgrades[floor(fmod($grade, 1) / 0.16)];
-            $ascend->climb->grade = $mainGrade . $subGrade;
-
-            return $ascend;
-        })->sortByDesc(fn($ascend) => $ascend->climb->grade)
-            ->groupBy(fn($ascend, $key) => (new Carbon($ascend->date_logged))->format('Y-m-d'))
-            ->sortKeysDesc();
-
-        $this->ascendsByDate = $groupedAscends;
-    }
-
-    public function getAscends(): Collection
+    public function getAscends($userId): Collection
     {
         return collect(
             (new TopLogger())->ascends()
                 ->filter(['used' => true])
-                ->filter(['user' => ['uid' => $this->userId]])
-                ->filter(['climb' => ['gym_id' => $this->gymId]])
+                ->filter(['user' => ['uid' => $userId]])
                 ->param(['serialize_checks' => true])
-                ->include(['climb' => ['hold']])
+                ->include(['climb'])
                 ->get()
         );
+    }
+
+    public function refresh(): void
+    {
+        Cache::clear();
+        $this->createDataset();
+    }
+
+    private function createDataset(): void
+    {
+        $this->ascendsByDate = collect();
+
+        foreach ($this->climberUids as $uid => $climber) {
+            $this->ascendsByDate = $this->ascendsByDate->merge(
+                Cache::rememberForever(
+                    'ascends' . $uid,
+                    fn() => $this->getAscends($uid)
+                )
+            );
+        }
+
+        // set grade to font & sort by grade
+        $this->ascendsByDate = $this->ascendsByDate
+            ->sortByDesc(fn($ascend) => $ascend->climb->grade)
+            ->map(function ($ascend) {
+                $grade = (float)$ascend->climb->grade;
+                $mainGrade = floor($grade);
+
+                $subGrade = $this->subgrades[floor(($grade - $mainGrade) / 0.16)] ?? '?';
+
+                $ascend->climb->grade = $mainGrade . $subGrade;
+                $ascend->climb->gym_name = $this->gyms[$ascend->climb->gym_id] ?? 'Nieuw!';
+
+                return $ascend;
+            });
+
+        // group and sort by date
+        $this->ascendsByDate = $this->ascendsByDate
+            ->groupBy(fn($ascend) => (new Carbon($ascend->date_logged))->format('Y-m-d'))
+            ->sortKeysDesc();
+
+        $this->ascendsByDate = $this->ascendsByDate->map(function (Collection $groupedAscends) {
+            return $groupedAscends->groupBy('user_id');
+        });
     }
 }
--- resources/views/livewire/boulders-by-date.blade.php
+++ resources/views/livewire/boulders-by-date.blade.php
@@ -1,24 +1,34 @@
 <div class="mx-2">
     <div class="h-10">
+        <x-button wire:click="refresh" wire:loading.attr="disabled">Refresh</x-button>
         <span wire:loading>Loading...</span>
     </div>
 
-    <x-button wire:click="$set('userId', '7163205870')" class="{{ $userId == '7163205870' ? 'font-bold' : '' }}">Ruben</x-button>
-    <x-button wire:click="$set('userId', '2943357766')" class="{{ $userId == '2943357766' ? 'font-bold' : '' }}">Wouter</x-button>
 
-    <br><br>
+    <div class="flex flex-col space-y-6">
+    @foreach($ascendsByDate as $date => $ascendsByUser)
+        <div>
+            <h3 class="text-xl">{{ $date }} - {{ $ascendsByUser->first()->first()->climb->gym_name }}</h3>
+            <div class="flex space-x-4">
+                @foreach($ascendsByUser as $userId => $ascends)
+                    <div class="w-32">
+                        <h4 class="text-lg">{{ $this->climberIds[$userId] }}</h4>
 
-    <x-button wire:click="$set('gymId', 8)" class="{{ $gymId == 8 ? 'font-bold' : '' }}">Bruut</x-button>
-    <x-button wire:click="$set('gymId', 3)" class="{{ $gymId == 3 ? 'font-bold' : '' }}">Monk</x-button>
-    <x-button wire:click="$set('gymId', 123)" class="{{ $gymId == 123 ? 'font-bold' : '' }}">Neoliet</x-button>
-    <x-button wire:click="$set('gymId', 131)" class="{{ $gymId == 131 ? 'font-bold' : '' }}">MN Dordt</x-button>
-
-    @foreach($ascendsByDate as $date => $ascends)
-        <h3 class="text-lg mt-2">{{ $date }}</h3>
-        <ol>
-            @foreach($ascends as $ascend)
-                <li class="ml-3 {{ $ascend->checks == 2 ? 'text-amber-500' : '' }}"> {{ $loop->index + 1 }}. {{ $ascend->climb->grade }}</li>
-            @endforeach
-        </ol>
+                        <div>
+                            <ol class="list-decimal ml-10" role="list">
+                                @foreach($ascends as $ascend)
+                                    <li class="{{ $ascend->checks == 2 ? 'text-amber-500' : '' }}">
+                                        {{ $ascend->climb->grade }}
+                                    </li>
+                                @endforeach
+                            </ol>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     @endforeach
+    </div>
+
 </div>
+
