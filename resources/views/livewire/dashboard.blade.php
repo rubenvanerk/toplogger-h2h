6de65a4d42a6be647dadf7ac25256e8bc42de1c7
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-04-11 16:46:29 +0200

    Change tab order

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -12,8 +12,8 @@
         </a>
 
         <a href="#" class="px-3 py-2 font-medium text-sm rounded-md"
-           x-on:click="currentTab = 'chart'" :class="currentTab == 'chart' ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700'">
-            Chart
+           x-on:click="currentTab = 'sessions'" :class="currentTab == 'sessions' ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700'">
+            Sessies
         </a>
 
         <a href="#" class="px-3 py-2 font-medium text-sm rounded-md"
@@ -22,8 +22,8 @@
         </a>
 
         <a href="#" class="px-3 py-2 font-medium text-sm rounded-md"
-           x-on:click="currentTab = 'sessions'" :class="currentTab == 'sessions' ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700'">
-            Sessies
+           x-on:click="currentTab = 'chart'" :class="currentTab == 'chart' ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700'">
+            Chart
         </a>
     </nav>
 
