69d6bad4aeb0698a3ab23972fc3511033799d8d7
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-09-11 11:51:30 +0200

    Migrate from Laravel Mix to Vite (#1)
    Co-authored-by: Shift <shift@laravelshift.com>

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
-
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
@@ -17,12 +16,12 @@
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+        @vite('resources/css/app.css')
         @livewireStyles
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
-        <script src="{{ url(mix('js/app.js')) }}" defer></script>
+        @vite('resources/js/app.js')
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
