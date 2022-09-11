4b32e612c39df514d55c554650db13d431c58cfe
Ruben van Erk <ruben@rubenvanerk.nl>
2022-09-11 12:14:54 +0200

    Remove debug

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -61,11 +61,9 @@ class Dashboard extends Component
 
     public function refreshData(): void
     {
-        Cache::put('updated_at', now()->subMinutes(5));
-        return;
-
         Cache::clear();
         $this->generateData();
+        Cache::put('updated_at', now()->subMinutes(5));
     }
 
     private function createAscendsByDate(): void
